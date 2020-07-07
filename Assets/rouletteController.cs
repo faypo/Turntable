f66cfabd05aece4b0f5b80793711dafc7a43da59
Faypo <king1532168@gmail.com>
2020-07-07 17:57:40 +0800

    [init] Roulette spinning

---
+++ Assets/rouletteController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rouletteController : MonoBehaviour
+{
+    public bool isStart, isStop;
+    public float mov, rotSpeed;
+
+    private void Start()
+    {
+        this.isStart = this.isStop = false;
+        this.mov = 0.1F;
+        this.rotSpeed = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.isStop = this.isStart ? true : false;
+            this.isStart = true;
+        }
+
+        if (this.isStart && !this.isStop)
+        {
+            if (this.rotSpeed < 30)
+            {
+                this.rotSpeed += this.mov;
+            }
+            else
+            {
+                this.rotSpeed = 30;
+            }
+        }
+        else if (this.isStart && this.isStop)
+        {
+            if (this.rotSpeed > 0)
+            {
+                this.rotSpeed -= this.mov;
+            }
+            else
+            {
+                this.rotSpeed = 0;
+                this.isStart = this.isStop = false;
+            }
+        }
+        //this.rotSpeed = 30;
+        
+        // if (Input.GetMouseButtonUp(0))
+        // {
+        //     this.rotSpeed = 0;
+        // }
+
+        //this.rotSpeed *= 0.9f;
+        transform.Rotate(0, 0, this.rotSpeed);
+        Debug.Log(this.rotSpeed);
+    }
+}
